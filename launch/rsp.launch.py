4e1505b2296bea2a9f5a0c415040dc2d7a6c93cc
chacha-eth <cha694158@gmail.com>
2025-10-31 13:51:02 +0000

    Created core URDF

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -32,10 +32,6 @@ def generate_launch_description():
 
     # Launch!
     return LaunchDescription([
-        DeclareLaunchArgument(
-            'use_sim_time',
-            default_value='false',
-            description='Use sim time if true'),
-
+        DeclareLaunchArgument('use_sim_time',default_value='false',description='Use sim time if true'),
         node_robot_state_publisher
     ])
